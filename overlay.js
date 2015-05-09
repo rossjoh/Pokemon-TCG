93f9d6047ff111dd2a57d3cb069a779a6461eeb1
rossjoh <rossjoh@hotmail.co.uk>
2015-05-09 21:03:15 +0100

    new overlay screens
    started new overlay screens when cards are clicked

--- overlay.js
+++ overlay.js
@@ -1,23 +1,50 @@
 function card_clicked(slot) //function is called when the card is clicked
 {
     var card_name = find_card_from_slot(slot);
+    console.log(slot)
+    if(slot.indexOf("my_hand") >= 0)
+    {
+        document.getElementById("popup_window_1").innerHTML = "<IMG src=\"images/" + card_name.card + ".jpg\" style=\"width:500px\" id=\"" + slot + "_image\" onclick=\"overlay_clicked();\">";
+        document.getElementById("popup_window_1").style.width  = "500px"
+        document.getElementById("popup_window_1").style.height = "700px"
+        document.getElementById("popup_window_1").style.left = "0px"
+        document.getElementById("popup_window_1").style.top = "0px"
+        document.getElementById("popup_window_1").style.position = "absolute"
+        $("#popup_window").fadeIn(200);
+        $("#popup_window_1").fadeIn(200);
+        $("#overlay").fadeIn(200);
+        document.getElementById("overlay").onclick = (function(){overlay_clicked();});
+    }
+    else if((slot.indexOf("my_active") >= 0) || (slot.indexOf("my_bench") >= 0))
+    {
+        document.getElementById("popup_window_1").innerHTML = "<IMG src=\"images/" + card_name.card + ".jpg\" style=\"width:500px\" id=\"" + slot + "_image\" onclick=\"overlay_clicked();\">";
+        document.getElementById("popup_window_1").style.width  = "500px"
+        document.getElementById("popup_window_1").style.height = "700px"
+        document.getElementById("popup_window_1").style.left = "0px"
+        document.getElementById("popup_window_1").style.top = "0px"
+        document.getElementById("popup_window_1").style.position = "absolute"
+        $("#popup_window").fadeIn(200);
+        $("#popup_window_1").fadeIn(200);
+        $("#overlay").fadeIn(200);
+        document.getElementById("overlay").onclick = (function(){overlay_clicked();});
+    }
+    else
+    {
+        document.getElementById("popup_window_1").innerHTML = "<IMG src=\"images/" + card_name.card + ".jpg\" style=\"width:500px\" id=\"" + slot + "_image\" onclick=\"overlay_clicked();\">";
+        document.getElementById("popup_window_1").style.width  = "500px"
+        document.getElementById("popup_window_1").style.height = "700px"
+        document.getElementById("popup_window_1").style.left = "0px"
+        document.getElementById("popup_window_1").style.top = "0px"
+        document.getElementById("popup_window_1").style.position = "absolute"
+        $("#popup_window").fadeIn(200);
+        $("#popup_window_1").fadeIn(200);
+        $("#overlay").fadeIn(200);
+        document.getElementById("overlay").onclick = (function(){overlay_clicked();});
+    }
     
-    //if(card_name )
-    
-    
-    document.getElementById("popup_window_1").innerHTML = "<IMG src=\"images/" + card_name.card + ".jpg\" style=\"width:500px\" id=\"" + slot + "_image\" onclick=\"overlay_clicked();\">";
-    document.getElementById("popup_window_1").style.width  = "500px"
-    document.getElementById("popup_window_1").style.height = "700px"
-    document.getElementById("popup_window_1").style.left = "0px"
-    document.getElementById("popup_window_1").style.top = "0px"
-    document.getElementById("popup_window_1").style.position = "absolute"
-    $("#popup_window").fadeIn(200);
-    $("#popup_window_1").fadeIn(200);
-    $("#overlay").fadeIn(200);
-    document.getElementById("overlay").onclick = (function(){overlay_clicked();});
 }
 
-function overlay_clicked()
+function overlay_clicked() //function is called when the overlay is clicked
 {
     $("#popup_window").fadeIn(200);
     $("#popup_window_1").fadeOut(200);
